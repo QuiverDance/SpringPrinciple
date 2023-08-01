74fb4aa60e5bf3e19526d548b34db73110d6a264
여름 <pjw7200@ajou.ac.kr>
2023-08-01 17:22:05 +0900

    ADD : singleton container test

---
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,29 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer(){
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService instance1 = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+
+        Assertions.assertThat(instance1).isSameAs(instance2);
+    }
+}
